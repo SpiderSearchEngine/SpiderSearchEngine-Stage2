87b24a1f1819d61bd21b1dbda8032df7a52e24f6
jairo <jairo@jairo-TOSHIBA-NB505>
2015-05-11 21:30:08 -0600

    Crea el Proyecto

---
+++ src/Logic/Main.java
@@ -0,0 +1,17 @@
+
+package Logic;
+
+/**
+ *
+ * @author jairo
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
